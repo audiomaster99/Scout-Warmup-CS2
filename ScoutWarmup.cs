a357a1ffbe2659c4eb39ea2b1ac4a2077a8531ae
audiomaster99 <116572799+audiomaster99@users.noreply.github.com>
2024-01-08 06:59:29 +0100

    Build 1

--- ScoutWarmup.cs
+++ ScoutWarmup.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using CounterStrikeSharp.API;
+﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
-using CounterStrikeSharp.API.Core.Listeners;
+using static CounterStrikeSharp.API.Core.Listeners;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Utils;
@@ -10,102 +9,100 @@ using CounterStrikeSharp.API.Modules.Menu;
 using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Modules.Timers;
 using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace ScoutWarmup
 {
-    public class ScoutWarmup : BasePlugin
-    {
-        public override string ModuleName => "Warmup Scout";
-        public override string ModuleVersion => "v.1";
-        public override string ModuleAuthor => "audio";
-        public override string ModuleDescription => "Enable Scout, Zeus, and Bhop during warmup";
 
-        private CCSGameRules? _gameRules = null;
+	public class ScoutWarmup : BasePlugin
+	{
+		public override string ModuleName => "Warmup Scout";
+		public override string ModuleVersion => "v.1";
+		public override string ModuleAuthor => "audio";
+		public override string ModuleDescription => "Scout + Zeus + Bhop in warmup";
 
-        public bool IsWarmup
-        {
-            get
-            {
-                if (_gameRules is null)
-                    InitializeGameRules();
+		CCSGameRules? _gameRules = null;   // ty Abner <3
 
-                return _gameRules is not null && _gameRules.WarmupPeriod;
-            }
-        }
+		public bool WarmupPeriod
+		{
+			get
+			{
+				if (_gameRules is null)
+					SetGameRules();
 
-        private void InitializeGameRules() => _gameRules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").FirstOrDefault()?.GameRules;
+				return _gameRules is not null && _gameRules.WarmupPeriod;
+			}
+		}
+		void SetGameRules() => _gameRules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").First().GameRules!;
 
-        public override void Load(bool hotReload)
-        {
-            OnMapStart(Server.MapName);
+		public override void Load(bool hotReload)
+		{
+			OnMapStart(Server.MapName);
 			RegisterListener<OnMapStart>(OnMapStart);
-        }
+		}
 
-        private void OnMapStart(string mapName)
+		void OnMapStart(string _mapname)
         {
             _gameRules = null;
-            AddTimer(1.0F, InitializeGameRules);
+            AddTimer(1.0F, SetGameRules);
         }
 
-        [GameEventHandler]
-        public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+		[GameEventHandler]
+		public HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
         {
-            if (IsWarmup)
-            {
-                foreach (var player in Utilities.GetPlayers())
+			if (WarmupPeriod)
+			{
+				foreach (var player in Utilities.GetPlayers())
                 {
-                    if (player == null || !player.IsValid || player.Connected != PlayerConnectedState.PlayerConnected)
+                    if(player == null && !player.IsValid || player.Connected != PlayerConnectedState.PlayerConnected)
                     {
                         // Skip invalid players
-                        continue;
+                        return HookResult.Continue;
                     }
-
-                    player.GiveNamedItem("weapon_ssg08");
-                    player.GiveNamedItem("weapon_taser");
-                }
-
-                Server.ExecuteCommand("sv_autobunnyhopping true");
+					player.GiveNamedItem("weapon_ssg08");
+					player.GiveNamedItem("weapon_taser");
+				}
+				Server.ExecuteCommand("sv_autobunnyhopping true");
                 Server.ExecuteCommand("sv_enablebunnyhopping true");
+				WriteColor($"[SCOUTWARMUP] - Warmup Started - Scout+Bhop Enabled", ConsoleColor.Green);
+				return HookResult.Continue;
+			}
+			return HookResult.Continue;
+		}
 
-                WriteColor("[SCOUTWARMUP] - Warmup Started - Scout+Bhop Enabled", ConsoleColor.Green);
-            }
-
-            return HookResult.Continue;
-        }
-
-        [GameEventHandler]
-        public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
-        {
-            if (IsWarmup)
-            {
-                Server.ExecuteCommand("sv_autobunnyhopping false");
+		[GameEventHandler]
+		public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
+		{  
+			if (WarmupPeriod)
+			{
+				Server.ExecuteCommand("sv_autobunnyhopping false");
                 Server.ExecuteCommand("sv_enablebunnyhopping false");
+				WriteColor($"[SCOUTWARMUP] - Warmup Ended - Scout+Bhop Disabled", ConsoleColor.Red);
+				return HookResult.Continue;
+			}
+			return HookResult.Continue;
+		}
 
-                WriteColor("[SCOUTWARMUP] - Warmup Ended - Scout+Bhop Disabled", ConsoleColor.Red);
-            }
-
-            return HookResult.Continue;
-        }
-
-        private static void WriteColor(string message, ConsoleColor color)
+		static void WriteColor(string message, ConsoleColor color)
         {
             var pieces = Regex.Split(message, @"(\[[^\]]*\])");
 
-            foreach (var piece in pieces)
+            for (int i = 0; i < pieces.Length; i++)
             {
+                string piece = pieces[i];
+
                 if (piece.StartsWith("[") && piece.EndsWith("]"))
                 {
                     Console.ForegroundColor = color;
-                    Console.Write(piece.Substring(1, piece.Length - 2));
-                    Console.ResetColor();
+                    piece = piece.Substring(1, piece.Length - 2);
                 }
-                else
-                {
-                    Console.Write(piece);
-                }
+
+                Console.Write(piece);
+                Console.ResetColor();
             }
 
             Console.WriteLine();
         }
-    }
+	}
 }
